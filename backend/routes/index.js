270c9f0069be20c8b10eb2e4451b4bcdddc56cca
arseth229 <146048786+arseth229@users.noreply.github.com>
2024-10-19 18:52:55 -0500

    frontend initial

--- backend/routes/index.js
+++ backend/routes/index.js
@@ -1,19 +1,47 @@
 // backend/routes/index.js
 const express = require('express');
 const router = express.Router();
-
 const apiRouter = require('./api');
 
-
-
-router.get("/api/csrf/restore", (req, res) => {
-  const csrfToken = req.csrfToken();
-  res.cookie("XSRF-TOKEN", csrfToken);
-  res.status(200).json({
-    'XSRF-Token': csrfToken
-  });
-});
-
 router.use('/api', apiRouter);
+
+if (process.env.NODE_ENV === 'production') {
+  const path = require('path');
+
+  router.get('/', (req, res) => {
+    res.cookie('XSRF-TOKEN', req.csrfToken());
+    res.sendFile(
+      path.resolve(__dirname, '../../frontend', 'dist', 'index.html')
+    );
+  });
+
+  router.use(express.static(path.resolve("../frontend/dist")));
+
+  router.get(/^(?!\/?api).*/, (req, res) => {
+    res.cookie('XSRF-TOKEN', req.csrfToken());
+    res.sendFile(
+      path.resolve(__dirname, '../../frontend', 'dist', 'index.html')
+    );
+  });
+}
+
+if (process.env.NODE_ENV !== 'production') {
+  router.get('/api/csrf/restore', (req, res) => {
+    res.cookie('XSRF-TOKEN', req.csrfToken());
+    return res.json({});
+  });
+}
+
+// if (process.env.NODE_ENV !== 'production') {
+//   router.get("/api/csrf/restore", (req, res) => {
+//     const csrfToken = req.csrfToken();
+//     res.cookie("XSRF-TOKEN", csrfToken);
+//     res.status(200).json({
+//       'XSRF-Token': csrfToken
+//     });
+//   });
+// }
+
+
 
 module.exports = router;
